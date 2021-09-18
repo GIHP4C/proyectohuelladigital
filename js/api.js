5370ea0fcdbd5f3420b981d18d49d2d6acf8f964
jgalanm07 <jgalanm07@hotmail.com>
2021-09-18 18:20:10 -0500

    Integracion del api de full contact 10%

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
+
+//Configuraciones API Instragram
 urlAPI2="https://instagram29.p.rapidapi.com/user/"
 var settingsAPI2 = {
 	"async": true,
@@ -10,24 +12,57 @@ var settingsAPI2 = {
 	}
 };
 
+//Configuraciones API Full Contact
+var settingsAPI1 = {
+    "url": "https://api.fullcontact.com/v3/person.enrich",
+    "method": "POST",
+	"async": true,
+	"crossDomain": true,
+    // This is the important part
+    /*"xhrFields": {
+        "withCredentials": true
+    },*/
+    "timeout": 0,
+    "headers": {
+      "Authorization": "Bearer GIbUNEeE0ZBei7W7ZzHMw6LK0iwfz7Md",
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    //"data": "{\r\n\"email\": \"snriedel85@gmail.com\"\r\n}",
+  };
 
-function consultarAPI2() {
-    const text=$("#usuario").val()
-    console.log(text)
-    if(!!text){
-        hiddenElements("btn2")
-        hiddenElements("usuario")
+
+async function consultarAPIs() {
+    const usuario=$("#usuario").val()
+    const correo=$("#correo").val()
+    let perfil={}
+    console.log(usuario, correo)
+    if(!!usuario || !!correo) {
+        hiddenElements("form")
+        //hiddenElements("usuario")
         showElements("loading2")
-        settingsAPI2.url=urlAPI2+text
-        $.ajax(settingsAPI2).done(function (response) {
+        settingsAPI2.url=urlAPI2+usuario
+        /*if(!!correo){
+            settingsAPI1['data']="{\r\n\"email\": \""+correo+"\"\r\n}"
+            console.log('Run api')
+            console.log(settingsAPI1)
+            await $.ajax(settingsAPI1).done(function (response) {
+                console.log(response);
+            });
+            console.log('API Done')
+        }*/
+        
+        await $.ajax(settingsAPI2).done(function (response) {
             console.log(response);
-            showElements("btn2")
-            showElements("usuario")
+            perfil['api_instragram']=response['data']
+            showElements("form")
+            //showElements("usuario")
             hiddenElements("loading2")
         }).fail(function (response) {
             console.error(response);
-            showElements("btn2")
-            showElements("usuario")
+            showElements("form")
+            //showElements("usuario")
             hiddenElements("loading2")
         });
     }else{
